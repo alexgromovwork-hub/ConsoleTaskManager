968a518854a7e26b2daa9605bcbe46ec4daa0257
alexgromovwork-hub <alex.gromov.work@gmail.com>
2026-01-17 01:00:29 +0200

    Addition 2

--- src/main/main.kt
+++ src/main/main.kt
@@ -7,14 +7,32 @@ enum class TaskStatus {
 
 data class Task(
     var task: String = "",
-    var status: TaskStatus= TaskStatus.TODO
+    var status: TaskStatus = TaskStatus.TODO
 )
 
 fun main(args: Array<String>) {
-    var TaskList = mutableListOf<String>().also {
+    var TaskList = mutableListOf<Task>().also {
         println("Hello, it's a task manager")
+    }
+    var ext = false
+    while (!ext) {
+        actionQuestion()//сначала блок вопроса что делать - показать список; добавить новое задание; удалить выбранное
+        printTaskList(TaskList) //если список пуст, то "Давайте создадим", если есть то "Вот ваш список н\ ${Список}"
+
 
     }
 
+
+}
+
+fun actionQuestion() {
+    val basicAnswers = listOf(
+        "1. Show task list",
+        "2. Add new task",
+        "3. Select and remove task",
+    )
+}
+
+fun printTaskList(taskList: MutableList<Task>) {
 
 }
