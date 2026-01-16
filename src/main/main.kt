5072117cd51d2220275a68f934628cb8318b414c
alexgromovwork-hub <alex.gromov.work@gmail.com>
2026-01-16 21:51:45 +0200

    Addition 1

--- src/main/main.kt
+++ src/main/main.kt
@@ -1,5 +1,20 @@
 package main
 
+enum class TaskStatus {
+    TODO,
+    DONE
+}
+
+data class Task(
+    var task: String = "",
+    var status: TaskStatus= TaskStatus.TODO
+)
+
 fun main(args: Array<String>) {
-    
+    var TaskList = mutableListOf<String>().also {
+        println("Hello, it's a task manager")
+
+    }
+
+
 }
